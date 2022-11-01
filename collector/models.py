cf3094f0e969f3d7a8002a3b508690962ab82846
Wrexan <fwrexan@gmail.com>
2022-11-01 20:08:53 +0200

    changed readme.md add task.md add models for user and user profile

--- collector/models.py
+++ collector/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    # user_id = models.PositiveIntegerField('Id', unique=True)
+    username = models.CharField('Username', max_length=64, unique=True)
+    password = models.CharField('Password', max_length=64)
+    first_name = models.CharField('First name', max_length=64)
+    last_name = models.CharField('Last name', max_length=64)
+    date_joined = models.DateTimeField('Joined at')
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE, verbose_name='User')
+    avatar_url = models.SlugField('Avatar', max_length=160)
+
+    class Meta:
+        verbose_name = "User's profile"
+        verbose_name_plural = "User's profiles"
