97d5d1ea094d27672a93d3e7bccab1c8765f35c7
Wrexan <fwrexan@gmail.com>
2022-11-02 06:17:52 +0200

    add check for uploaded files number and extensions add messaging to template and errors about file checks

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.generic import View, FormView
 
 from .forms import FileFieldForm
@@ -30,12 +32,25 @@ class UsersUploadFileFieldFormView(FormView):
         form = self.get_form(form_class)
         files = request.FILES.getlist('files')
         if form.is_valid():
-            self.handle_uploaded_files(files)
+            self.handle_uploaded_files(request, files)
             return super().form_valid(form)
         else:
             print(f'form is invalid: {form.__dict__=}')
             return self.form_invalid(form)
 
-    def handle_uploaded_files(self, files):
-        print(f'{files=}')
-        pass
+    def handle_uploaded_files(self, request, files):
+        errors = {"csv": "Expected CSV and XML file. CSV absent",
+                  "xml": "Expected CSV and XML file. XML absent"}
+        files_by_ext = {}
+        if len(files) != 2:
+            errors['AMT'] = f"There must be 2 files, you're trying to upload {len(files)}"
+        for file in files:
+            extension = file.name.lower()[-3:]
+            if extension in errors:
+                files_by_ext[extension] = file
+                del errors[extension]
+        if errors:
+            [messages.error(request, message) for message in errors.values()]
+            return
+
+
