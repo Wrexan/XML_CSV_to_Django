2638d8cb9ec48ba8eaccae3523ecc0933993238a
Wrexan <fwrexan@gmail.com>
2022-11-03 03:46:34 +0200

    closed access to everything except login\logout for non admin users

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View, FormView
 
 from .forms import FileFieldForm
@@ -8,14 +8,19 @@ from logic.users_logic import UserUploader
 
 class UsersView(View):
     def get(self, request):
-        users = User.objects.all()
+        if request.user.is_superuser:
+            users = User.objects.all()
+        else:
+            users = None
         return render(request, "users/users.html", {"user_list": users})
 
 
 class UserProfileView(View):
     def get(self, request, pk):
-        user_profile = User.objects.get(id=pk)
-        return render(request, "users/user_profile.html", {"user_profile": user_profile})
+        if request.user.is_superuser:
+            user_profile = User.objects.get(id=pk)
+            return render(request, "users/user_profile.html", {"user_profile": user_profile})
+        return redirect('/')
 
 
 class UsersUploadFileFieldFormView(FormView):
@@ -24,15 +29,19 @@ class UsersUploadFileFieldFormView(FormView):
     success_url = "/"
 
     def get(self, request, *args, **kwargs):
-        return render(request, "users/upload.html")
+        if request.user.is_superuser:
+            return render(request, "users/upload.html")
+        return redirect('/')
 
     def post(self, request, *args, **kwargs):
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
-        files = request.FILES.getlist('files')
-        if form.is_valid():
-            uploader = UserUploader()
-            uploader.handle_uploaded_files(request, files)
-            return super().form_valid(form)
-        else:
-            return self.form_invalid(form)
+        if request.user.is_superuser:
+            form_class = self.get_form_class()
+            form = self.get_form(form_class)
+            files = request.FILES.getlist('files')
+            if form.is_valid():
+                uploader = UserUploader()
+                uploader.handle_uploaded_files(request, files)
+                return super().form_valid(form)
+            else:
+                return self.form_invalid(form)
+        return redirect('/')
